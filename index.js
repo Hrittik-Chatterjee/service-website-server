5bc3d88009d57d13a014bf14a1ed32e0472e71e3
Hrittik-Chatterjee <@hrittik0chatterjee@gmail.com>
2022-11-19 01:46:15 +0600

    modified

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express =require('express')
 const cors =require('cors')
 const app = express()
 const port =process.env.PORT || 5000
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const e = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+// const e = require('express');
+
+app.use(cors())
+app.use(express.json())
+
+
+
 require('dotenv').config()
 
 app.get('/',(req,res)=>{
@@ -22,14 +28,33 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run (){
+        // try{
+        //     const userCollection = client.db('doctorService').collection('users')
+        //     const user = {
+        //         name :'testing test',
+        //         email :'testing@gmail.com'
+        //     }
+        //     const result = await userCollection.insertOne(user)
+        //     console.log(result)
+        // }
+        // finally{
+
+        // }
         try{
-            const userCollection = client.db('doctorService').collection('users')
-            const user = {
-                name :'testing test',
-                email :'testing@gmail.com'
-            }
-            const result = await userCollection.insertOne(user)
-            console.log(result)
+            const serviceCollection = client.db('doctorService').collection('services')
+            app.get('/services', async(req,res)=>{
+                const query ={}
+                const cursor = serviceCollection.find(query)
+                const services = await cursor.toArray()
+                res.send(services)
+            })
+
+           app.get('/services/:id', async(req, res)=>{
+            const id =req.params.id
+            const query={_id: ObjectId(id)}
+            const service = await serviceCollection.findOne(query)
+            res.send(service)
+           })
         }
         finally{
 
@@ -40,8 +65,7 @@ run().catch(err => console.log(err))
 
 
 
-app.use(cors())
-app.use(express.json())
+
 
 
 
